6e570125eea44029c5aaf2c627cb3e8d486a9a71
hernando <hernandozambrano556@gmail.com>
2025-08-24 21:39:48 -0500

    Se agrega Transaccionalidad a la creación del Usuario

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/pragma/r2dbc/UserHandler.java
@@ -0,0 +1,21 @@
+package co.com.pragma.r2dbc;
+
+import co.com.pragma.model.user.User;
+import co.com.pragma.usecase.user.UserUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.reactive.TransactionalOperator;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserHandler {
+
+    private final UserUseCase userUseCase;
+    private final TransactionalOperator transactionalOperator;
+
+    public Mono<User> saveUser(User user) {
+        return userUseCase.saveUser(user)
+                .as(transactionalOperator::transactional);
+    }
+}
